e1147103cec0fba2001e40db2cb32a6ec3e8a782
Iamneo <techadmin@iamneo.ai>
2023-11-13 09:46:00 +0000

    Backup Commit

--- AngularDemo/src/app/Pipes/tds.pipes.ts
+++ AngularDemo/src/app/Pipes/tds.pipes.ts
@@ -1,15 +1,14 @@
-import{Pipe,PipeTransform} form '@angular/core'
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name:"tds"
+name:'tds'
 })
-
 export class TDS implements PipeTransform
 {
-    {{1000 | currency:'INR'}}
+    
 
-    transform(value :any,...args,:any[]){
-        let salary:number=value*(value*.10);
+    transform(value: any, ... args: any[]) {
+        let salary:number = value- (value * .10);       
 
         return salary;
     }
