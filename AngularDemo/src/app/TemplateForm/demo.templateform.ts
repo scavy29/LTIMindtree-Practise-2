c75812ef555fcdc66e25b48867bb5bf0698d731c
Iamneo <techadmin@iamneo.ai>
2023-11-13 06:30:27 +0000

    Backup Commit

---
+++ AngularDemo/src/app/TemplateForm/demo.templateform.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'templateforms',
+    templateUrl:'./demo.templateforms.html'
+})
+export class TemplateForm{
+
+}
--- AngularDemo/src/app/app.module.ts
+++ AngularDemo/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SamplecompoComponent } from './samplecompo/samplecompo.component';
-import { TemplateFormComponent } from './template-form/template-form.component';
+import { TemplateForm } from './TemplateForm/demo.templateform';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SamplecompoComponent,
-    TemplateFormComponent
+    TemplateForm
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
