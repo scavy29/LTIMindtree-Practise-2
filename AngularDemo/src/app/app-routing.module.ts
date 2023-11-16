b47815164b3c2d4e247dcb07a2f4e4e2602439bd
Iamneo <techadmin@iamneo.ai>
2023-11-16 09:38:38 +0000

    Backup Commit

--- AngularDemo/src/app/app-routing.module.ts
+++ AngularDemo/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DemoRoute } from './demoRoute.component';
+import { ReactiveForm } from './ReactiveForm/reactiveForm.component';
 
 
 const routes: Routes = [
-  {path: '' , component:DemoRoute  }
+  {path: '' , component:DemoRoute  },
+  {path:'reactive',component:ReactiveForm}
 ];
 
 @NgModule({
