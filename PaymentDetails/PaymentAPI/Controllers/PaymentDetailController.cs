811011d10d8f42205d3bd68e366110af8f88253e
Iamneo <techadmin@iamneo.ai>
2023-11-17 19:22:21 +0000

    Backup Commit

--- PaymentDetails/PaymentAPI/Controllers/PaymentDetailController.cs
+++ PaymentDetails/PaymentAPI/Controllers/PaymentDetailController.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace PaymentAPI.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PaymentDetailController : ControllerBase
     {
@@ -27,20 +27,50 @@ namespace PaymentAPI.Controllers
         }
 
         [HttpGet]
-        [Route("ListAllPayments")]
-        public IActionResult ListAllPayments()
+        public IActionResult GetPaymentDetail()
         {
-            var listofpay=db.PaymentDetails;
+            if(db.PaymentDetails==null)
+            {
+                return NotFound();
+            }
+            return Ok(db.PaymentDetails);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPaymentDetail(int id)
+        {
+            if(db.PaymentDetails==null)
+            {
+                return NotFound();
+            }
+            var gpd=db.PaymentDetails.Find(id);
+            if()   
+        }
+
+        [HttpPost]
+        public IActionResult PostPaymentDetail(PaymentDetail pd)
+        {
+            if(db.PaymentDetails==null)
+            {
+                return NotFound();
+            }
+            db.PaymentDetails.Add(pd);
+            db.SaveChanges();
             return Ok();
         }
 
         [HttpPost]
-        [Route("CreatePayment")]
-        public IActionResult CreatePayment(PaymentDetail pd)
+        [Route("{id}")]
+        public IActionResult UpdatePaymentDetail(int id,PaymentDetail pd)
         {
-            db.PaymentDetails.Add(pd);
-            db.SaveChanges();
-            return Ok();
+            var u=db.PaymentDetails.Find(id);
+            if(u!=null)
+            {
+                db.PaymentDetails.Update(u);
+                db.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
         }
 
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
