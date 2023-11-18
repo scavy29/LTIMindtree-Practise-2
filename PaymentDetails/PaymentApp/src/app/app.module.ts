7ad6641615610a207ba151f0764c4e22b383a433
Iamneo <techadmin@iamneo.ai>
2023-11-18 04:47:35 +0000

    Backup Commit

---
+++ PaymentDetails/PaymentApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PaymentDetailsComponent } from './payment-details/payment-details.component';
+import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
