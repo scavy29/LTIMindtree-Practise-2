87f81ed90507734dd290103283501155f753a052
Iamneo <techadmin@iamneo.ai>
2023-11-18 11:14:16 +0000

    Backup Commit

--- PaymentDetails/PaymentApplication/src/app/paymentcrud/paymentcrud.component.ts
+++ PaymentDetails/PaymentApplication/src/app/paymentcrud/paymentcrud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-paymentcrud',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./paymentcrud.component.css']
 })
 export class PaymentcrudComponent implements OnInit {
+  PaymentArray : any[] = [];
+  isResultLoaded=false;
+  isUpdateFormActive=false;
+  
+  CardOwnerName:string="";
+  CardNumber:string="";
+  ExpirationDate:string="";
+  SecurityCode:string="";
 
-  constructor() { }
+  PaymentDetailId=""
 
-  ngOnInit() {
+  constructor(private http:HttpClient) {
+    this.getallpayments();
+   }
+
+  ngOnInit():void{
+  }
+
+  getallpayments()
+  {
+    this.http.get("https://8080-bdedfececadfabcaaaceeafebecebbffdafdefabcc.premiumproject.examly.io/api/PaymentDetail")
+    .subscribe((resultData:any)=>
+    {
+      this.isResultLoaded=true;
+      console.log(resultData);
+      this.PaymentArray=resultData;
+    });
   }
 
 }
