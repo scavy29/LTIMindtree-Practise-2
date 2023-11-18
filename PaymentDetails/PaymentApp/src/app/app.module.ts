886b592431eb77ff950633c5c6f15213598fd07c
Iamneo <techadmin@iamneo.ai>
2023-11-18 03:47:34 +0000

    Backup Commit

--- PaymentDetails/PaymentApp/src/app/app.module.ts
+++ PaymentDetails/PaymentApp/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PaymentDetailsComponent } from './payment-details/payment-details.component';
-import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PaymentDetailsComponent,
-    PaymentDetailFormComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule
--- PaymentDetails/PaymentApp/src/app/shared/payment-detail.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PaymentDetail } from './payment-detail.model';
-
-describe('PaymentDetail', () => {
-  it('should create an instance', () => {
-    expect(new PaymentDetail()).toBeTruthy();
-  });
-});
