d59b5567d2c5418c98cda4e7a0c5994cd4d5bd65
Iamneo <techadmin@iamneo.ai>
2023-11-18 11:29:15 +0000

    Backup Commit

--- PaymentDetails/PaymentApplication/src/app/app.module.ts
+++ PaymentDetails/PaymentApplication/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentcrudComponent } from './paymentcrud/paymentcrud.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
--- PaymentDetails/PaymentApplication/src/app/paymentcrud/paymentcrud.component.ts
+++ PaymentDetails/PaymentApplication/src/app/paymentcrud/paymentcrud.component.ts
@@ -1,39 +1,107 @@
-import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+// import {}
 
 @Component({
   selector: 'app-paymentcrud',
   templateUrl: './paymentcrud.component.html',
-  styleUrls: ['./paymentcrud.component.css']
+  styleUrls: ['./paymentcrud.component.scss']
 })
-export class PaymentcrudComponent implements OnInit {
-  PaymentArray : any[] = [];
-  isResultLoaded=false;
-  isUpdateFormActive=false;
-  
-  CardOwnerName:string="";
-  CardNumber:string="";
-  ExpirationDate:string="";
-  SecurityCode:string="";
+export class PaymentCrudComp {
+  StudentArray : any[] = [];
+  isResultLoaded = false;
+  isUpdateFormActive = false;
 
-  PaymentDetailId=""
+  stname: string ="";
+  course: string ="";
+ 
+  currentStudentID = "";
 
-  constructor(private http:HttpClient) {
-    this.getallpayments();
-   }
-
-  ngOnInit():void{
+  constructor(private http: HttpClient ) 
+  {
+    this.getAllPayments();
   }
 
-  getallpayments()
-  {
+  ngOnInit(): void {
+  }
+
+  getAllPayments()
+  { 
     this.http.get("https://8080-bdedfececadfabcaaaceeafebecebbffdafdefabcc.premiumproject.examly.io/api/PaymentDetail")
-    .subscribe((resultData:any)=>
+    .subscribe((resultData: any)=>
     {
-      this.isResultLoaded=true;
-      console.log(resultData);
-      this.PaymentArray=resultData;
+        this.isResultLoaded = true;
+        console.log(resultData);
+        this.PaymentArray = resultData;
     });
   }
 
+  // register()
+  // {
+  //  // this.isLogin = false; 
+  //  // alert("hi");
+  //   let bodyData = {
+  //     "stname" : this.stname,
+  //     "course" : this.course,
+    
+  //   };
+
+    // this.http.post("https://localhost:7195/api/Student/AddStudent",bodyData).subscribe((resultData: any)=>
+    // {
+    //     console.log(resultData);
+    //     alert("Payment Registered Successfully")
+    //     this.getAllPa();
+      //  this.name = '';
+      //  this.address = '';
+      //  this.mobile  = 0;
+  //   });
+  // }
+
+  setUpdate(data: any) 
+  {
+   this.CardOwnerName = data.CardOwnerName;
+   this.CardNumber = data.CardNumber;
+   this.ExpirationDate = data.ExpirationDate;
+   this.SecurityCode=data.SecurityCode
+  }
+
+  // UpdateRecords()
+  // {
+  //   let bodyData = 
+  //   {
+  //     "stname" : this.stname,
+  //     "course" : this.course,
+  //   };
+    
+  //   this.http.patch("https://localhost:7195/api/Student/UpdateStudent"+ "/"+ this.currentStudentID,bodyData).subscribe((resultData: any)=>
+  //   {
+  //       console.log(resultData);
+  //       alert("Student Registered Updateddd")
+  //       this.getAllStudent();
+      
+  //   });
+  // }
+ 
+  save()
+  {
+    if(this.PaymentDetailId == '')
+    {
+        this.register();
+    }
+      else
+      {
+       this.UpdateRecords();
+      }       
+  }
+
+
+  // setDelete(data: any)
+  // {
+  //   this.http.delete("https://localhost:7195/api/Student/DeleteStudent"+ "/"+ data.id).subscribe((resultData: any)=>
+  //   {
+  //       console.log(resultData);
+  //       alert("Student Deletedddd")
+  //       this.getAllStudent();
+  //   });
+  // }
 }
