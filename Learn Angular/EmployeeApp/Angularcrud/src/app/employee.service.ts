080ca6e2eb979492eed27a1b9ad0f8f683a9c74b
Iamneo <techadmin@iamneo.ai>
2023-11-19 12:06:29 +0000

    Backup Commit

---
+++ Learn Angular/EmployeeApp/Angularcrud/src/app/employee.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+}
