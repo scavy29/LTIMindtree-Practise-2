bd178f960281439a4c5edadf6ff1c612f9a6564f
Iamneo <techadmin@iamneo.ai>
2023-11-19 13:58:33 +0000

    Backup Commit

--- Learn Angular/EmployeeApp/Angularcrud/src/app/employee/employee.component.ts
+++ Learn Angular/EmployeeApp/Angularcrud/src/app/employee/employee.component.ts
@@ -13,10 +13,77 @@ import { Employee } from '../employee';
 export class EmployeeComponent implements OnInit {
   dataSaved=false;
   employeeForm:any;
-  
-  constructor() { }
+  allEmployees:Observable<Employee[]>;
+  employeeIdUpdate=null;
+  massage=null;
+
+  constructor(private formbuilder: FormBuilder, private employeeService:EmployeeService) { }
 
   ngOnInit() {
   }
+  loadAllEmployees(){
+    this.allEmployees = this.employeeService.getAllEmployee();
+  }
 
+  onFormSubmit(){
+    this.dataSaved=false;
+    const employee = this.employeeForm.value;
+    this.CreateEmployee(employee);
+    this.employeeForm.reset();
+  }
+
+  loadEmployeeToEdit(employeeId: string){
+    this.employeeService.getEmployeeById(employeeId).subscribe(employee=> {
+      this.massage=null;
+      this.dataSaved=false;
+      this.employeeIdUpdate=employee.EmpId;
+      this.employeeForm.controls['EmpName'].setValue(employee.EmpName);
+      this.employeeForm.controls['DateOfBirth'].setValue(employee.DateOfBirth);
+      this.employeeForm.controls['EmailId'].setValue(employee.EmailId);
+      this.employeeForm.controls['Gender'].setValue(employee.Gender);
+      this.employeeForm.controls['Address'].setValue(employee.Address);
+      this.employeeForm.controls['PinCode'].setValue(employee.PinCode);
+    });
+  }
+
+  CreateEmployee(employee: Employee){
+    if(this.employeeIdUpdate==null){
+      this.employeeService.createEmployee(employee).subscribe(
+        ()=>{
+          this.dataSaved=true;
+          this.massage="Record Saved Successfully";
+          this.loadAllEmployees();
+          this.employeeIdUpdate=null;
+          this.employeeForm.reset();
+        }
+      );
+    }
+    else{
+      employee.EmpId = this.employeeIdUpdate;
+      this.employeeService.updateEmployee(employee).subscribe(()=>{
+        this.dataSaved=true;
+        this.massage="Record Updated Successfully";
+        this.loadAllEmployees();
+        this.employeeIdUpdate=null;
+        this.employeeForm.reset();
+      });
+    }
+  }
+
+  deleteEmployee(employeeId: string){
+    if(confirm("Are you Sure you want to delete ?")){
+      this.employeeService.deleteEmployeeById(employeeId).subscribe(()=>{
+        this.dataSaved=true;
+        this.massage="Record deleted Successfully";
+        this.loadAllEmployees();
+        this.employeeIdUpdate=null;
+        this.employeeForm.reset();
+      });
+    }
+  }
+  resetForm(){
+    this.employeeForm.reset();
+    this.massage=null;
+    this.dataSaved=false;
+  }
 }
