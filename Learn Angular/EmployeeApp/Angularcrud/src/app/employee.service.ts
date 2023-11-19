982c9c0ebcacc37a05a294709adf4ebb86904bdf
Iamneo <techadmin@iamneo.ai>
2023-11-19 12:43:33 +0000

    Backup Commit

--- Learn Angular/EmployeeApp/Angularcrud/src/app/employee.service.ts
+++ Learn Angular/EmployeeApp/Angularcrud/src/app/employee.service.ts
@@ -15,7 +15,23 @@ export class EmployeeService {
   }
 
   getEmployeeById(employeeId: string): Observable<Employee>{
-    return this.http.get<Employee[]>(this.url)
+    return this.http.get<Employee>(this.url+'/GetEmployeeById' + employeeId);
+  }
+  
+  createEmployee(employee: Employee): Observable<Employee>{
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type':'applications/json'})};
+    return this.http.post<Employee>(this.url + '/AddEmployee',employee, httpOptions);
+  }
+
+  updateEmployee(employee: Employee): Observable<Employee>{
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type':'application/json'})};
+    return this.http.put<Employee>(this.url + '/UpdateEmployee',employee, httpOptions);
+  }
+
+  deleteEmployeeById(employeeid: string): Observable<number>{
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type':'application/json'})};
+    return this.http.put<number>(this.url + '/DeleteEmployee?id' + employeeid, httpOptions);
   }
   
 }
+
