c99c35157b45a967c0bcaea508a18dae4c20a5dc
Iamneo <techadmin@iamneo.ai>
2023-11-19 11:36:13 +0000

    Backup Commit

--- Learn Angular/EmployeeApp/CrudAngular/Controllers/CRUDAPIController.cs
+++ Learn Angular/EmployeeApp/CrudAngular/Controllers/CRUDAPIController.cs
@@ -17,12 +17,14 @@ namespace CrudAngular.Controllers
             this.db=_context;
         }
         
+        //GetAllEmployees
         [HttpGet]
         public IActionResult GetEmployee()
         {
             return Ok(db.Employees);
         }
 
+        //GetEmployeeWithID
         [HttpGet]
         [Route("GetEmployeeById/{id}")]
         public IActionResult GetEmployeeById(int empid)
@@ -35,9 +37,49 @@ namespace CrudAngular.Controllers
             return NotFound();
         }
 
-        public IActionResult PostEmployee(Employee e)
+        //Add an Employee
+        [HttpPost]
+        public IActionResult AddEmployee(Employee e)
         {
-            
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            db.Employees.Add(e);
+            db.SaveChanges();
+            return Ok(e);
+        }
+
+        //Update an Employee
+        [HttpPut]
+        public IActionResult UpdateEmployee(int id,Employee emp)
+        {
+            var e=db.Employees.Find(id);
+            if(e!=null)
+            {
+                e.EmpName=emp.EmpName;
+                e.DateOfBirth=emp.DateOfBirth;
+                e.EmailId=emp.EmailId;
+                e.Gender=emp.Gender;
+                e.Address=emp.Address;
+                e.pinCode=emp.pinCode;
+                db.SaveChanges();
+                return Ok(emp);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var e=db.Employees.Find(id);
+            if(e!=null)
+            {
+                db.Employees.Remove(e);
+                db.SaveChanges();
+                return Ok(e);
+            }
+            return BadRequest();
         }
     }
 }
