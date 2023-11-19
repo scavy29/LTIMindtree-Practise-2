d8bcf447744440fdd9990813922a4384923da01f
Iamneo <techadmin@iamneo.ai>
2023-11-19 13:13:33 +0000

    Backup Commit

--- Learn Angular/EmployeeApp/Angularcrud/src/app/app.module.ts
+++ Learn Angular/EmployeeApp/Angularcrud/src/app/app.module.ts
@@ -1,6 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { EmployeeService } from './employee.service';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatRadioModule} from '@angular/material/radio';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -12,9 +26,24 @@ import { EmployeeComponent } from './employee/employee.component';
   ],
   imports: [
     BrowserModule,
+    EmployeeService,
+    MatMenuModule,
+    MatTooltipModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClient,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatIconModule,
+    MatRadioModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatFormFieldModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [HttpClientModule,EmployeeService,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
