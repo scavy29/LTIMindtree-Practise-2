e2f79467f45fdc0cbc6a52c7e592829c46e8ee2e
Iamneo <techadmin@iamneo.ai>
2023-11-25 05:52:15 +0000

    Backup Commit

--- MileStone/Laundry/angularapp/src/app/schedule-details/schedule-details.component.ts
+++ MileStone/Laundry/angularapp/src/app/schedule-details/schedule-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ScheduleService } from '../services/schedule-service.service';
 
 @Component({
   selector: 'app-schedule-details',
@@ -8,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class ScheduleDetailsComponent implements OnInit {
 
   items:any[]
-  
-  constructor() { }
+
+  constructor(private s:ScheduleService) { }
 
   ngOnInit(): void {
+    this.s.getSchedules().subscribe(
+      (data)=>
+      this.items=data
+    )
   }
-
 }
