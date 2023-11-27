218cd2f684e1c8e3add06c23c2e594c1d4fbd57c
Iamneo <techadmin@iamneo.ai>
2023-11-27 06:05:09 +0000

    Backup Commit

--- MovieApplication(Angular)/MovieApplication/src/app/menu/menu.component.ts
+++ MovieApplication(Angular)/MovieApplication/src/app/menu/menu.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  templateUrl: './menu.component.html',
+  // templateUrl: './menu.component.html',
+  template: `<p>hello</p>`,
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
