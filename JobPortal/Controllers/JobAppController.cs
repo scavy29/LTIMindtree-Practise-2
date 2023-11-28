d4f912935e99d7c86f354257a56289cf4ae32249
Iamneo <techadmin@iamneo.ai>
2023-11-28 19:23:12 +0000

    Backup Commit

--- JobPortal/Controllers/JobAppController.cs
+++ JobPortal/Controllers/JobAppController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JobPortal.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobPortal.Controllers
 {
@@ -28,8 +29,8 @@ namespace JobPortal.Controllers
         */
 
         // PUT: api/Job/{Id}
-        [HttpPut("api/Job/{Id}")]
-public IActionResult UpdateJob(int Id, Job updatedJob)
+    [HttpPut("api/Job/{Id}")]
+public async Task<IActionResult> UpdateJob(int Id, Job updatedJob)
 {
     try
     {
@@ -39,7 +40,7 @@ public IActionResult UpdateJob(int Id, Job updatedJob)
             return BadRequest(ModelState);
         }
 
-        var existingJob = _context.Jobs.FirstOrDefault(j => j.JobID == Id);
+        var existingJob = await _context.Jobs.FirstOrDefaultAsync(j => j.JobID == Id);
 
         if (existingJob != null)
         {
@@ -51,7 +52,7 @@ public IActionResult UpdateJob(int Id, Job updatedJob)
             existingJob.DeadLine = updatedJob.DeadLine;
             existingJob.Category = updatedJob.Category;
 
-            _context.SaveChanges();
+            await _context.SaveChangesAsync(); // Use asynchronous SaveChanges method
             return Ok(existingJob);
         }
 
@@ -64,29 +65,32 @@ public IActionResult UpdateJob(int Id, Job updatedJob)
 }
 
 
+
         //GET: api/Job
         [HttpGet]
-        [Route("api/Job")]
-        public async Task<IActionResult> GetJobs()
-        {
-            try
-            {
-                var jobs = _context.Jobs;
-                return Ok(jobs);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
-        }
+[Route("api/Job")]
+public async Task<IActionResult> GetJobs()
+{
+    try
+    {
+        var jobs = await _context.Jobs.ToListAsync(); // Assuming you are using Entity Framework Core
+
+        return Ok(jobs);
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, $"Internal server error: {ex.Message}");
+    }
+}
+
 
         // DELETE: api/Job/{Id}
         [HttpDelete("api/Job/{Id}")]
-        public IActionResult DeleteJob(int id)
+        public IActionResult DeleteJob(int Id)
         {
             try
             {
-                var jid=_context.Jobs.Find(id);
+                var jid=_context.Jobs.Find(Id);
                 if(jid!=null)
                 {
                     _context.Remove(jid);
@@ -104,61 +108,68 @@ public IActionResult UpdateJob(int Id, Job updatedJob)
 
         // GET: api/Job/JobTitle
         [HttpGet]
-        [Route("api/Job/JobTitle")]
-        public IActionResult GetJobTitle(string title)
+[Route("api/Job/JobTitle")]
+public async Task<IActionResult> GetJobTitle(string title)
+{
+    try
+    {
+        var positionTitles = await _context.Jobs.FirstOrDefaultAsync(j => j.JobTitle == title);
+
+        if (positionTitles != null)
         {
-            try
-            {
-                var positionTitles = _context.Jobs.FirstOrDefault(j=>j.JobTitle==title);
-                return Ok(positionTitles);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
-            }
+            return Ok(positionTitles);
         }
+
+        return NotFound($"No job found with the title '{title}'.");
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, $"Internal server error: {ex.Message}");
+    }
+}
 
         //POST: api/Job
         [HttpPost]
-        [Route("api/Job")]
-        public async Task<IActionResult> AddJob([FromBody] Job j)
+[Route("api/Job")]
+public async Task<IActionResult> AddJob([FromBody] Job j)
+{
+    try
+    {
+        if (j == null)
+        {
+            return BadRequest("Job position data is invalid.");
+        }
+
+        _context.Jobs.Add(j);
+        await _context.SaveChangesAsync();
+
+        return Ok(j);
+        //return CreatedAtAction("GetJobPosition", new { id = jobPosition.Id }, jobPosition);
+    }
+    catch (Exception ex)
+    {
+        return StatusCode(500, $"Internal server error: {ex.Message}");
+    }
+}
+
+
+        // DELETE: api/Job/{Id}
+        [HttpDelete("positions/delete")]
+        public async Task<IActionResult> DeleteJobPositions()
         {
             try
             {
-                if (j == null)
-                {
-                    return BadRequest("Job position data is invalid.");
-                }
-
-                _context.Jobs.Add(j);
+                var jobPositions = await _context.Jobs.ToListAsync();
+                _context.Jobs.RemoveRange(jobPositions);
                 await _context.SaveChangesAsync();
+                return Ok(jobPositions);
 
-                return Ok(j);
-                //return CreatedAtAction("GetJobPosition", new { id = jobPosition.Id }, jobPosition);
+                //return NoContent();
             }
             catch (Exception ex)
             {
                 return StatusCode(500, $"Internal server error: {ex.Message}");
             }
         }
-
-    
-        // [HttpDelete("positions/delete")]
-        // public async Task<IActionResult> DeleteJobPositions()
-        // {
-        //     try
-        //     {
-        //         var jobPositions = await _context.JobPositions.ToListAsync();
-        //         _context.JobPositions.RemoveRange(jobPositions);
-        //         await _context.SaveChangesAsync();
-        //         return Ok(jobPositions);
-
-        //         //return NoContent();
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         return StatusCode(500, $"Internal server error: {ex.Message}");
-        //     }
-        // }
     }
 }
